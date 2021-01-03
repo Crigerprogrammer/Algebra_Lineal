785c4be0e79bb3a962179c155dec1419400a2b30
Cristian <crigerprogrammer@gmail.com>
2021-01-03 13:39:31 -0600

    ejercicio

--- producto_escalar_vector.py
+++ producto_escalar_vector.py
@@ -4,3 +4,10 @@ vector = np.array([1,-1,1]) # Declaramos el vector como numpy array
 escalar = 3.1 # Declaramos el escalar
 
 print('El resultado del producto vector y escalar es ', vector*escalar)
+
+### EJERCICIO ###
+v = np.array([1,1])
+a = -1
+b = 2
+c = 0.1
+
