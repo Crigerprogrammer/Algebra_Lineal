9cf17a5f579f3160a1be1eac443e1eca5a9db601
Cristian <crigerprogrammer@gmail.com>
2021-01-02 18:01:04 -0600

    numpy suma

--- suma_vectores_numpy.py
+++ suma_vectores_numpy.py
@@ -3,6 +3,7 @@ import numpy as np
 rojo = [255,0,0]
 verde = [0,255,0]
 azul = [0,0,255]
+negro = [0,0,0]
 
 # Numpy tiene una estructura de datos llamado numpy arrays y se pueden crear con la propiedad array
 # Los numpy array tienen las propiedades de un vector y su suma puede ser como los vectores algebraicos
@@ -11,3 +12,13 @@ verde = np.array(verde)
 azul = np.array(azul)
 
 print('La suma de los numpy array rojo mas verde es: ', rojo+verde)
+
+# Ejercicio de clase #6
+# vector a = [0,0,255] es el color azul
+
+# Color al sumar rojo verde y azul
+print('La suma de los vectores, rojo, verde y azul es: ', rojo+verde+azul)
+# Color sumando rojo y verde
+print('La suma de los vectores, rojo y verde es: ', rojo+verde)
+# Color sumando negro - azul es:
+print('La suma negro menos azul es: ', negro-(+azul))
