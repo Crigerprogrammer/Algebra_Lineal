fbb4a4364b76cf4b0e4758c4b6426146ae102628
Cristian <crigerprogrammer@gmail.com>
2021-01-03 13:23:12 -0600

    python

---
+++ producto_escalar_vector.py
@@ -0,0 +1,6 @@
+import numpy as np # Importamos numpy
+
+vector = np.array([1,-1,1]) # Declaramos el vector como numpy array
+escalar = 3.1 # Declaramos el escalar
+
+print('El resultado del producto vector y escalar es ', vector*escalar)
