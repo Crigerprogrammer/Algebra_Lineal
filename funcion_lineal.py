33a131549c8996cff8291f7d0988c82ebd0296c8
Cristian <crigerprogrammer@gmail.com>
2021-01-04 10:37:55 -0600

    funcion lineales python

--- funcion_lineal.py
+++ funcion_lineal.py
@@ -3,5 +3,11 @@ import numpy as np
 def f(x):
     return x[0]+x[1]+x[2]+x[3]
 
-def fnumpy(x)
+def fnumpy(x):
     return np.sum(x)
+
+a = np.array([1,1,1,1])
+b = np.array([1,0,1,0])
+x,y = 1,-2
+
+print('f(x*a + y*b) = ', f(x*a + y*b))
